3ab4d9f4eddf9a3cc08dfd125900ee2532b821bc
yuutan0518 <iwasaku3309@gmail.com>
2020-04-22 14:15:11 +0900

    設定ファイルの更新

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.force_ssl = true
-
 end
